75eb3bf6e5a86f37955155766009ff5edb8595b4
cbboipdx@gmail.com <ctharve@gmail.com>
2014-05-14 18:08:10 -0400

    added loops for taking the median across transcripts for a particular gene

--- GxE_assemble_tables.R
+++ GxE_assemble_tables.R
@@ -1,44 +1,47 @@
 ##################################################################
 ## Chris Harvey
 ##
-## May 14, 2013
+## Assemble a Complete matric of GxE data
 ##
 ##################################################################
-plates <- 1:12
-plates <- plates[-c(3, 6, 9)]
+plate_seq <- 1:12
+plates <- paste('P', plate_seq[-c(3, 6, 9)], sep='')
 
-invisible(
-	sapply(plates, function(ii){
-		#ii <-1	
-		cat(paste('Plate ', ii, sep=''))
-		cat('\n')
-	
-		load(paste('./P', ii, '/counts/P', ii, '.data.Rd', sep=''))
+all_data <- invisible(
+            	lapply(plates, function(ii){
+            		#ii <-1	
+            		cat(paste('Plate: ', ii, sep=''))
+            		cat('\n')
+            	
+            		load(paste('./', ii, '/counts/', ii, '.data.Rd', sep=''))
 
-		cat(paste('Colnames & Transcript.IDs identical? ', identical(rownames(data), anno$t.id), sep=''))
-		cat('\n')
-		cat(paste('Annotation transcript count: ', dim(anno)[1], sep=''))
-		cat('\n')
+            		cat(paste('Colnames & Transcript.IDs identical? ', identical(rownames(data), anno$t.id), sep=''))
+            		cat('\n')
+            		cat(paste('Annotation transcript count: ', dim(anno)[1], sep=''))
+            		cat('\n')
 
-		data <- data.frame(data)
-		data$ensg <- anno$ensg
+            		data <- data.frame(data)
+            		data$ensg <- anno$ensg
 
-		assign(paste('data.P', ii, sep=''), data, envir=globalenv())
-
-		assign('anno', anno, envir=globalenv())
-		rm(data)
-
-	})
+            		data
+            	})
 )
+names(all_data) <- plates 
+
+gene_data <- invisible(
+                lapply(all_data, function(this_data){
+                    this_data <- all_data[[1]]
+
+                    unique_genes <- unique(this_data$ensg)
+                    
 
 
-
-
-
+                })
+)
 
 
 unique_genes <- unique(data.P1$ensg)
 
 
 
-format(object.size(data.P1), units='MB')
+format(object.size(all_data), units='MB')
