c70c0cf80be8f35a806cb97cd4fe1ccbed3fd872
cbboipdx@gmail.com <ctharve@gmail.com>
2014-05-30 16:33:34 -0400

    include cellLine and treatment field in table output

--- counts_QC.R
+++ counts_QC.R
@@ -15,46 +15,58 @@ if(length(cargs)>=1)
 if(length(cargs)>=2)
   bamDir <- cargs[2]
 
+## for debugging
+#platePrefix <- "D1P4"
+bamDir <- "../../bams"
 
-#platePrefix <- "D1P1"
-#bamDir <- "../../bams"
+## Extract the covariates file to match cell-lines and treaments with barcodes.
+cov.file <- paste('../../../covariates/GxE_', platePrefix, '_covariates.txt', sep='')
+covariates <- read.table(cov.file, header=TRUE, sep='\t')
+cov_short <- covariates[, c('Plate.ID', 'Barcode.ID', 'CellLine', 'Treatment.ID')]
 
+## Function takes a bam directory, a bam prefix(which filtered bams), and a plate prefix to return a matrix of counts
 CountBams <- function(bamDir, bamPrefix, platePrefix){
     inCommand <- paste("ls ", bamDir, "/*", bamPrefix, ".bam", sep='')
     outFile <- paste(platePrefix, "_", bamPrefix, "_reads.txt", sep='')
     outCommand <- paste(inCommand, " | while read f; do echo $f `samtools view -c $f`; done", " > ", outFile, sep='')
     cat(outCommand)
     system(outCommand)
-
     return(outFile)
 }
 
+## Bams filters: Raw reads merged across lanes, filtered for -q10, and filtered -q10 with duplicates removed
 filter_options <- c('merged', 'quality', 'clean')
 
+## apply the CountBams function across all filter levels
 counts <- lapply(filter_options, function(filter_level){
 #        filter_level <- filter_options[1]
         outFile <- CountBams(bamDir, filter_level, platePrefix)
 
         temp <- read.table(paste('./', outFile, sep=''), as.is=TRUE)
         ## nice reg exp example
-        barcodes <- as.numeric(sub('_.*','',sub(".*HT","",temp$V1)))
+        Barcode.ID <- as.numeric(sub('_.*','',sub(".*HT","",temp$V1)))
         counts <- temp[, 2]
 
-        data.frame(barcodes, counts, stringsAsFactors=FALSE)
+        data.frame(Barcode.ID, counts, stringsAsFactors=FALSE)
         })
 
+## Reduce the output from the 
 ## barcode X filterLevel table
-counts_merged <- Reduce(function(...){merge(..., by='barcodes')}, counts)
-names(counts_merged) <- c('barcodes', filter_options)
-write.table(counts_merged, file=paste(platePrefix, '_QC_counts.txt', sep=''), quote=FALSE, row.names=FALSE, sep='\t')
+counts_merged <- Reduce(function(...){merge(..., by='Barcode.ID')}, counts)
+#counts_merged <- read.table('DP2_QC_counts.txt', header=TRUE, sep='\t') ## debugging
+#counts_merged <- counts_merged[, -c(2,3)] ## debugging
+names(counts_merged) <- c('Barcode.ID', filter_options) 
+counts_final <- merge(cov_short, counts_merged, by='Barcode.ID')
+counts_final <- counts_final[, c('Plate.ID', 'Barcode.ID', 'CellLine', 'Treatment.ID', 'merged', 'quality', 'clean')]
+
+write.table(counts_final, file=paste(platePrefix, '_QC_counts.txt', sep=''), quote=FALSE, row.names=FALSE, sep='\t')
 
 ## prepare plot data
 counts <- c(counts_merged$merged, counts_merged$quality, counts_merged$clean)
-barcodes <- rep(counts_merged$barcodes, 3)
-n.barcodes <- length(counts_merged$barcodes)
+barcodes <- rep(counts_merged$Barcode.ID, 3)
+n.barcodes <- length(counts_merged$Barcode.ID)
 filter <- c(rep('initial', n.barcodes), rep('quality', n.barcodes), rep('clean', n.barcodes))
 dat <- data.frame(barcodes=as.factor(barcodes), filter=as.factor(filter), counts)
-
 dat$filter <- factor(dat$filter, levels=c('initial', 'quality', 'clean'))
 
 ## plot
