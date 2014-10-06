509eee0d57d0a30f21ab0b97a575de02cc25872a
cbboipdx@gmail.com <ctharve@gmail.com>
2014-10-06 12:17:00 -0400

    generalized batch submission scripts for QuASAR

--- QuASAR_pipeline/QuASAR_pipeline.sh
+++ QuASAR_pipeline/QuASAR_pipeline.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 plate=$1
 cellLine=$2
+script=$3
 ncpus=2
 queue=mmtxq
 jobName=${plate}-${cellLine}
 
-echo "cd ${PWD}; Rscript QuASAR_pipeline.R ${plate} ${cellLine}" | qsub -q ${queue} -l nodes=1:ppn=${ncpus} -N ${jobName} -o ${jobName}.Qsub -e ${jobName}.Qsub.e 
+echo "cd ${PWD}; Rscript ${script} ${plate} ${cellLine}" | qsub -q ${queue} -l nodes=1:ppn=${ncpus} -N ${jobName} -o ${jobName}.Qsub -e ${jobName}.Qsub.e 
