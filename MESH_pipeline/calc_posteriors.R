051569109daf7ce9e1172450ee6233f6344268e4
cbboipdx@gmail.com <ctharve@gmail.com>
2015-01-22 12:58:48 -0500

    calc_posteriors in matrix algebra

--- MESH_pipeline/calc_posteriors.R
+++ MESH_pipeline/calc_posteriors.R
@@ -6,7 +6,8 @@
 ##
 #####################################################
 ## check default ncpus for myParallel.R
-source('~/piquelab/charvey/source/myRScripts/myParallel.R')
+#source('~/piquelab/charvey/source/myRScripts/myParallel.R')
+library(reshape2)
 
 parmfiles <- scan(pipe('ls *.hm_config.est'), character(0))
 bffiles <- scan(pipe('ls *.hm_config.in'), character(0))
@@ -28,6 +29,8 @@ for(ii in 1:n.samples){
   config3 <- as.numeric(scan(pipe(paste("less ", parmf, " | awk '$1==\"config:\" {print $6}'", sep='')),character(0)))
   config <- c(config1, config2, config3)
 
+  config <- c(0.1,0.1,0.8)
+
   priors <- c(pi00 * config, pi0)
   sum(priors)
 
@@ -36,38 +39,43 @@ for(ii in 1:n.samples){
   grid3 <- as.numeric(scan(pipe(paste("less ", parmf, " | awk '$1==\"grid:\" {print $6}'", sep='')),character(0)))
   grid <- c(grid1, grid2, grid3)
 
-  data <- read.table(bffile)
+  data <- read.table(bffile, as.is=TRUE)
   names(data) <- c('rsID', 'config', 'bf1', 'bf2', 'bf3')
 
-  snps <- as.character(unique(data[, 'rsID']))
+  data$abfs <- 10^as.matrix(data[, c('bf1', 'bf2', 'bf3')]) %*% grid
+  bfs <- cbind(acast(data, rsID ~ config ,value.var="abfs"), 1)
+  const <- 1/ (bfs %*% priors)
 
-  bfs_post <- t(ParallelSapply(snps, function(snp){
-    #snp <- snps[1]
-    temp <- subset(data, rsID==snp, select=c('bf1', 'bf2', 'bf3'))
-    bf1 <- (10^temp[1, ]) %*% grid
-    bf2 <- (10^temp[2, ]) %*% grid
-    bf3 <- (10^temp[3, ]) %*% grid
-    bfs <- c(bf1, bf2, bf3, 1)
-    const <- 1/sum((bfs*priors))
-    post <- bfs*priors*const
- 
-    return(c(bfs, post))
-  }))
-  bfs_names <- c('bf_c1', 'bf_c2', 'bf_c3', 'bf_c4')
-  post_names <- c('pos_c1', 'pos_c2', 'pos_c3', 'pos_c4')
-  colnames(bfs_post) <- c(bfs_names, post_names)
+  post <- sapply(1:4,function(col){
+    bfs[,col] * priors[col] * const
+  })
 
-  bfs <- data.frame(bfs_post[, bfs_names], stringsAsFactors=FALSE, row.names=NULL)
-  post <- data.frame(bfs_post[, post_names], stringsAsFactors=FALSE, row.names=NULL)
+  MyPaste <- function(...){paste(..., sep='_')}
+  split_name <- function(name)
+  {
+    names_snps <- strsplit(name, '_')[[1]][1:5]
+    name_out <- Reduce(MyPaste, names_snps)
+    name_out
+  }
+  
+  names_snps <- sapply(rownames(bfs), split_name, USE.NAMES=FALSE)
 
-  myDir <- './posteriors_bfs_parallel'
+  rownames(post) <- names_snps
+  rownames(bfs) <- names_snps
+  colnames(bfs) <- c('bf_c1', 'bf_c2', 'bf_c3', 'bf_c4')
+  colnames(post) <- c('post_c1', 'post_c2', 'post_c3', 'post_c4')
+
+  bfs_out <- data.frame(bfs, stringsAsFactors=FALSE)
+  post_out <- data.frame(post, stringsAsFactors=FALSE)
+
+  myDir <- './posteriors_bfs'
   system(paste0('mkdir -p ', myDir))
   root <- gsub('.hm_config.est', '', parmf)
 
-  write.table(post, file=paste(myDir, '/', root, '_posteriors.txt', sep=''), quote=FALSE, row.names=FALSE, sep='\t')
-  write.table(bfs, file=paste(myDir, '/', root, '_bayesFactors.txt', sep=''), quote=FALSE, row.names=FALSE, sep='\t')
+  write.table(post_out, file=paste(myDir, '/', root, '_posteriors.txt', sep=''), quote=FALSE, row.names=TRUE, sep='\t')
+  write.table(bfs_out, file=paste(myDir, '/', root, '_bayesFactors.txt', sep=''), quote=FALSE, row.names=TRUE, sep='\t')
 
+}
 ##         ##
 ## THE END ##
 ##         ##
-}
